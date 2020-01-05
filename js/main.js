d8ed88ccc9bafa73bbdb5ee3578a2a492e958629
Stepan <lukachina.stepan@mail.com>
2020-01-05 19:51:43 +0200

    btn

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ $(document).ready( function() {
 		$('.button-inner-item').css('display', 'none');	
 		$('.button-inner-item-close').css('display', 'block');
 		$('.button-inner-item-telegram, .button-inner-item-viber').css('display', 'block');
+		$('.backgroundcolor').css('display', 'block');
 			});
 			
 			$('#btn_contacts-close').click( function() {
 		$('.button-inner-item').css('display', 'block');	
 		$('.button-inner-item-close').css('display', 'none');
 		$('.button-inner-item-telegram, .button-inner-item-viber').css('display', 'none');
+			$('.backgroundcolor').css('display', 'none');
 			});
 
 	
